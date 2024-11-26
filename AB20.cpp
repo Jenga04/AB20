85155584be316d45a51957fa91b39ebec5351b9f
Jenga04 <jeremyja@msmk.university>
2024-11-26 14:46:14 +0100

    Agregar archivos de proyecto.

---
+++ AB20.cpp
@@ -0,0 +1,76 @@
+﻿// AB20.cpp: define el punto de entrada de la aplicación.
+//
+#include "AB20.h"
+#include "Windows.h"
+#include <string>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class servicio{
+private:
+	int idServicio;
+	string tipoServicio;
+};
+
+servicio::servicio(){
+
+}
+
+servicio::~servicio(){
+}
+class Paciente{
+private:
+	int idPaciente;
+	string nombre;
+	DATE fechaIngreso;
+
+
+};
+
+Paciente::Paciente(){
+}
+
+Paciente::~Paciente(){
+}
+class Médico{
+private:
+	int idMédico;
+	string nombre;
+	string especialidad;
+	boolean disponibilidad;
+};
+
+Médico::Médico(){
+}
+
+Médico::~Médico()
+{
+}
+class cita{
+private:
+	int idCita;
+	DATE fecha;
+	clock_t hora;
+};
+
+cita::cita()
+{
+}
+
+cita::~cita()
+{
+}
+class Hospital{
+private:
+
+
+};
+
+Hospital::Hospital()
+{
+}
+
+Hospital::~Hospital()
+{
+}
