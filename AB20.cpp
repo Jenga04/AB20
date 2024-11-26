ff5aea16e16c8cd8cae5f55a660c57c5437340d1
Jenga04 <jeremyja@msmk.university>
2024-11-26 17:09:54 +0100

    Constructores y métodos

--- AB20.cpp
+++ AB20.cpp
@@ -4,20 +4,28 @@
 #include "Windows.h"
 #include <string>
 #include <iostream>
+#include <ctime>
+#include <vector>
 using namespace std;
 
-class servicio{
-private:
-	int idServicio;
-	string tipoServicio;
-};
-class paciente{
+class paciente {
 private:
 	int idPaciente;
 	string nombre;
-	DATE fechaIngreso;
-
-
+	string fechaIngreso;
+	string historial;
+public:
+	paciente(int id, string n, string fecha): 
+		idPaciente(id),
+		nombre(n),
+		fechaIngreso(fecha),
+		historial("") {}
+	void mostrarDatos() {
+		cout << "ID del paciente: " << idPaciente << ".\n Nombre: " << nombre << ".\n Fecha de ingreso: " << fechaIngreso << endl;
+	}
+	void actualizarHistorial(string nuevoHistorial) { historial = nuevoHistorial; }
+	int getId() { return idPaciente; }
+	string getNombre() { return nombre; }
 };
 
 class médico{
@@ -25,26 +33,102 @@ private:
 	int idMédico;
 	string nombre;
 	string especialidad;
-	boolean disponibilidad;
+	bool disponibilidad;
+public:
+	médico (int id, string n, string tipo, bool disponible): 
+		idMédico(id), 
+		nombre(n), 
+		especialidad(tipo), 
+		disponibilidad(true) {}
+	void mostrarDatos() {
+		cout << "ID del médico: " << idMédico << ".\n Nombre: " << nombre << ".\n Especialidad: " << especialidad << ".\n Disponibile:  " << (disponibilidad ? "Si" : "No") << endl;
+	}
+	void cambiarDisponibilidad(bool estado) { disponibilidad = estado; }
+	int getID() { return idMédico; }
+	string getEspecialidad() { return especialidad; }
 };
 
-class cita{
+class cita {
 private:
 	int idCita;
 	string fechaHora;
-	int urgencia;
 	int idPaciente;
 	int idMédico;
+	int urgencia;
+public:
+	cita (int id, string fecha, int idp, int idm, int urgente):
+		idCita(id),
+		fechaHora(fecha),
+		idPaciente(idp),
+		idMédico(idm) {
+		if (urgencia < 1) {
+			this-> urgencia = 1;
+			cout << "La urgencia es mínima.\n";
+		}
+		else if (urgencia >5) {
+			this-> urgencia = 5;
+			cout << "La urgencia es máxima.\n";
+		}
+		else {
+			this-> urgencia = urgencia;
+		}
+	}
+	void mostrarDatos() {
+		cout << "ID de cita: " << idCita << ".\n Fecha y hora: " << fechaHora << ".\n ID del paciente: " << idPaciente << ".\n ID del médico: " << idMédico << ". \n Urgencia: " << urgencia << endl;
+	}
+	string getFechaHora() { return fechaHora; }
+	int getUrgencia() { return urgencia; }
+
 };
 
-class Hospital{
+class servicio {
 private:
-	vector<paciente> listaPacientes;
-	vector<médico> listaMédicos;
-	vector<cita> listaCitas;
-	vector<servicio> listaServicios;
+	int idServicio;
+	string tipoServicio;
 public:
-	void registrarPacientes(
-		int id, 
-		string nombre, 
-		DATE fecha)
+	servicio(int id, string tipo): 
+		idServicio(id), 
+		tipoServicio(tipo) {}
+	void mostrarDatos() {
+		cout << "ID de Servicio: " << idServicio << ".\n Tipo de servicio: " << tipoServicio << endl;
+	};
+};
+
+class hospital {
+private:
+	vector <paciente> listaPacientes;
+	vector <médico> listaMédicos;
+	vector <cita> listaCitas;
+	vector <servicio> listaServicios;
+public:
+	void registrarPaciente(int id, string nombre, string fecha) {
+		listaPacientes.push_back(paciente(id, nombre, fecha));
+		cout << "Paciente registrado. \n";
+	}
+	void listadoPacientes() {
+		cout << "Lista de pacientes: \n";
+		for (auto& paciente : listaPacientes) {
+			paciente.mostrarDatos();
+		}
+	}
+	void registrarMédico(int id, string n, string tipo, bool disponible) {
+		listaMédicos.push_back(médico(id, n, tipo, disponible));
+		cout << "Médico registrado. \n";
+	}
+	void listadoMédicos() {
+		cout << "Lista de médicos: \n";
+		for (auto& médico : listaMédicos) {
+			médico.mostrarDatos();
+		}
+	}
+	void asignarCita(int id, string fecha, int idp, int idm, int urgente) {
+		listaCitas.push_back(cita(id, fecha, idp, idm, urgente));
+		cout << "Cita asignada. \n";
+	}
+	void listadoCitas() {
+		cout << "Lista de citas: \n";
+		for (auto& cita : listaCitas) {
+			cita.mostrarDatos();
+		}
+	}
+};
