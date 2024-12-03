a0b412e7924de68fb66bfd6edabc1b0506e9e07e
Jenga04 <jeremyja@msmk.university>
2024-12-03 21:17:26 +0100

    Paciente.H

--- AB20.h
+++ AB20.h
@@ -1,8 +1,13 @@
 ﻿// AB20.h: archivo de inclusión para archivos de inclusión estándar del sistema,
 // o archivos de inclusión específicos de un proyecto.
-
 #pragma once
-
 #include <iostream>
 
+class paciente {
+private:
+	int idPaciente;
+	string nombre;
+	string fechaIngreso;
+	string historial;
+};
 // TODO: Haga referencia aquí a los encabezados adicionales que el programa requiere.
