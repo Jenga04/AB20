1192b311318bbf901495d2034493e30a860e7bfd
Jenga04 <jeremyja@msmk.university>
2025-01-14 17:38:46 +0100

    Cita.cpp

--- Cita.cpp
+++ Cita.cpp
@@ -2,3 +2,39 @@
 #include <iostream>
 using namespace std;
 
+cita::cita(int id, string fecha, int idp, int idm, int urgente) :
+	idCita(id),
+	fechaHora(fecha),
+	idPaciente(idp),
+	idMedico(idm) {
+	if (urgente < 1) {
+		this->urgencia = 1;
+	}
+	else if (urgente > 5) {
+		this->urgencia = 5;
+	}
+	else {
+		this->urgencia = urgente;
+	}
+}
+
+int cita::getIdCita() {
+	return idCita;
+}
+string cita::getFechaHora() {
+	return fechaHora;
+}
+int cita::getIdPaciente() {
+	return idPaciente;
+}
+int cita::getIDMedico() {
+	return idMedico;
+}
+int cita::getUrgencia() {
+	return urgencia;
+}
+
+void cita::datosCitas() {
+	cout << "Id de la cita: " << idCita << "\nFecha y hora: " << fechaHora << "\nID del paciente: " << idPaciente << "\n ID del medico: " << idMedico << "\nNivel de urgencia: " << urgencia;
+}
+
--- Cita.h
+++ Cita.h
@@ -9,31 +9,12 @@ private:
 	int idMedico;
 	int urgencia;
 public:
-	cita(int id, string fecha, int idp, int idm, int urgente) :
-		idCita(id),
-		fechaHora(fecha),
-		idPaciente(idp),
-		idMedico(idm) {
-		this->urgencia = (urgencia < 1) ? 1 : (urgencia > 5) ? 5 : urgencia;
-	}
+	cita(int id, string fecha, int idp, int idm, int urgente);
 
-	void datosCitas() {
-		cout << "ID de cita: " << idCita << ".\nFecha y hora: " << fechaHora << ".\nID del paciente: " << idPaciente << ".\nID del médico: " << idMedico << ". \nUrgencia: " << urgencia << endl;
-	}
-
-	int getIdPaciente() {
-		return idPaciente;
-	}
-	int getIDMedico(){
-		return idMedico;
-		}
-	string getFechaHora() { 
-		return fechaHora; 
-	}
-	void setFechaHora(const string& nuevaFecha) {
-		fechaHora = nuevaFecha;
-	}
-	int setUrgencia() { 
-		return urgencia; 
-	}
+	int getIdCita();
+	string getFechaHora();
+	int getIdPaciente();
+	int getIDMedico();
+	int getUrgencia();
+	void datosCitas();
 };
